0385995582f8c03be0bc3115b30575a381f6739f
DESKTOP-E0AE5J4\NAMUIT <20521631@gm.uit.edu.vn>
2022-11-07 23:55:34 +0700

    Init repo

---
+++ app/src/main/java/com/example/vmtestbuild/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.vmtestbuild;
+
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.vmtestbuild.Utils;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textView = (TextView) findViewById(R.id.show);
+
+        Button build = (Button) findViewById(R.id.build);
+        build.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Utils.hasEmulatorBuild(textView, getApplicationContext());
+            }
+        });
+
+    }
+}
